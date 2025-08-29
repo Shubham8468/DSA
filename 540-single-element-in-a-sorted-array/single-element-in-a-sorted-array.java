3089736bbe9fd7ee8826b59a96648cd73f974fcf
Shubham <shubh386a@gmail.com>
2025-08-29 11:37:50 +0530

    Time: 0 ms (100.00%) | Memory: 50.8 MB (14.53%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,22 +1,38 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        Stack<Integer> s=new Stack<>();
-        if(nums.length==1){
-            return nums[0];
+    public int singleNonDuplicate(int[] arr) {
+        int n=arr.length;
+        int lo=0,hi=n-1;
+        if(arr.length==1){
+            return arr[0];
         }
-        for(int i=0;i<=nums.length-1;i++){
-            s.push(nums[i]);
-            if(i==nums.length-1){
-                return nums[i];
+        if(arr[0]!=arr[1]){
+            return arr[0];
+        }
+        if(arr[n-2]!=arr[n-1]){
+            return arr[n-1];
+        }
+        
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+                return arr[mid];
             }
-            if(s.peek()==nums[i+1]){
-                i=i+1;
+            int f=mid,s=mid;
+            if(arr[mid-1]==arr[f]){
+                f=mid-1;
             }
             else{
-                return nums[i];
+                s=mid+1;
             }
-
+            int leftCount=f-lo;
+            int rightCount=hi-s;
+            if(leftCount%2==0){
+                lo=s+1;
+            }
+            else{
+                hi=f-1;
+            }
         }
-        return -1;
+        return 0;
     }
 }
