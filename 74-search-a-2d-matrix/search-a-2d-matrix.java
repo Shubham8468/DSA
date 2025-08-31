522a52709be2bcb9ba2f6e26624a9c3dd8f1ac9a
Shubham <shubh386a@gmail.com>
2025-08-31 16:42:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (50.15%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int tar) {
+        int row=arr.length;
+        int col=arr[0].length;
+        int lo=0;
+        int hi=row*col-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int midrow=mid/col;
+            int midcol=mid%col;
+            if(arr[midrow][midcol]==tar){
+                return true;
+            }
+            else if (arr[midrow][midcol]>tar){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return false;
+        
+    }
+}
