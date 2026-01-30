fe558184e9700c01d963a6786de33a664e7b2eee
Shubham <shubh386a@gmail.com>
2026-01-30 21:46:51 +0530

    Time: 19 ms (12.00%) | Memory: 46.7 MB (13.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,40 +1,41 @@
 class Solution {
     public boolean isAnagram(String s1, String s2) {
-        int s1l=s1.length();
-        int s2l=s2.length();
-        if(s1l!=s2l)return false;
-        HashMap<Character,Integer> map1= new HashMap<>();
-        for(int i=0;i< s1.length();i++){
-            char ch = s1.charAt(i);
-            if(map1.containsKey(ch)){
-                int freq= map1.get(ch);
-                map1.put(ch,freq+1);
-            }
-            else{
-                map1.put(ch,1);
-            }
-        }
-        HashMap<Character,Integer> map2=new HashMap<>();
-        for(int i=0;i< s2.length();i++){
-            char ch= s2.charAt(i);
-            if(!map1.containsKey(ch)){
-                return false;
-            }
-            if(map2.containsKey(ch)){
-                int freq=map2.get(ch);
-                map2.put(ch,freq+1);
-            }
-            else{
-                map2.put(ch,1);
-            }
-        }
-        for(char ch : map2.keySet()){
-            int freq1=map1.get(ch);
-            int freq2=map2.get(ch);
-            if(freq1!=freq2){
-                return false;
-            }
-        }
-        return true; 
+         int l1= s1.length();
+     int l2=s2.length();
+     if(l1!=l2){
+         return false;
+     }
+        HashMap<Character,Integer> map1=new HashMap<>();
+     for(int i=0;i<l1;i++){
+         char ch = s1.charAt(i);
+         if(map1.containsKey(ch)){
+             int freq= map1.get(ch);
+             map1.put(ch,freq+1);
+         }
+         else{
+             map1.put(ch,1);
+         }
+     }
+     HashMap<Character,Integer> map2= new HashMap<>();
+     for(int i=0;i<l2;i++){
+         char ch = s2.charAt(i);
+         if(!map1.containsKey(ch))return false;
+
+         if(map2.containsKey(ch)){
+             int freq= map2.get(ch);
+             map2.put(ch,freq+1);
+         }
+         else{
+             map2.put(ch,1);
+         }
+     }
+      for( char ch : map2.keySet()){
+          int freq1= map1.get(ch);
+          int freq2= map2.get(ch);
+          if(freq2< freq1){
+              return false;
+          }
+      }
+      return true;
     }
 }
