a5fd670af177d639b19698af0534f4499ccdb818
Shubham <shubh386a@gmail.com>
2026-01-30 21:05:19 +0530

    Time: 5 ms (77.16%) | Memory: 46.6 MB (5.05%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -1,17 +1,27 @@
 class Solution {
     public int countKDifference(int[] arr, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int ele : arr){
-           map.put(ele,map.getOrDefault(ele,0)+1);
+      HashMap<Integer,Integer> map=new HashMap<>();
+      for(int ele : arr){
+        if(map.containsKey(ele)){
+            int freq=map.get(ele);
+            map.put(ele,freq+1);
         }
-        int pair=0;
-        for(int ele : map.keySet()){
-            int rem1=ele-k;
-            int rem2=ele+k;
-            if(map.containsKey(rem1)) pair+=(map.get(ele)* map.get(rem1));
-             if(map.containsKey(rem2)) pair+=(map.get(ele)* map.get(rem2));
+        else{
+            map.put(ele,1);
         }
-        pair/=2;
-        return pair;
+      }
+      int pairs=0;
+      for(int ele : map.keySet()){
+        int rem1=ele-k;
+        int rem2=ele+k;
+        if(map.containsKey(rem1)) {
+            pairs+= (map.get(rem1)*map.get(ele));
+        }
+        if(map.containsKey(rem2)){
+            pairs+= (map.get(rem2)* map.get(ele));
+        }
+      }  
+       pairs/=2;
+       return pairs;
     }
 }
