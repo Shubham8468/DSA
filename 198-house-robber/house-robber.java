c59830ae21b678bf21e765d08e9457d736e21f97
Shubham <shubh386a@gmail.com>
2026-02-11 07:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (73.94%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,20 @@
 class Solution {
-    public int amout(int[] nums, int i , int[] dp){
+    public int amout(int[] nums, int i ,int[] dp){
         if(i>=nums.length)return 0;
         if(dp[i]!=-1){
             return dp[i];
         }
         int take=nums[i]+amout(nums,i+2,dp);
-        int skip=amout(nums, i+1,dp);
-        
-        return dp[i]=Math.max(take,skip);
+        int skip=amout(nums,i+1,dp);
+        dp[i]= Math.max(take,skip);
+        return dp[i];
     }
-    public int rob(int[] nums) {
 
-        int[] dp=new int[nums.length];
+
+    public int rob(int[] nums) {
+        int[] dp= new int[nums.length];
         Arrays.fill(dp,-1);
         return amout(nums,0,dp);
+        
     }
 }
