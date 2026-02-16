c954ba69b31bcc154f22e9743cf21d78bd0738dc
Shubham <shubh386a@gmail.com>
2026-02-16 06:03:59 +0530

    Time: 4 ms (5.14%) | Memory: 50.3 MB (5.36%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        Stack<Character> st= new Stack<>();
+        for(char i : s){
+            st.push(i);
+        }
+        for(int i=0 ; i<s.length;i++){
+            s[i]=st.pop();
+        }
+
+       
+    }
+}
