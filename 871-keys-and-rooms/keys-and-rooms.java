2becddc6397e4696fce3325e375b1e44eea728df
Shubham <shubh386a@gmail.com>
2026-03-09 18:58:27 +0530

    Time: 1 ms (85.98%) | Memory: 46.3 MB (80.73%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,23 +1,21 @@
 class Solution {
-   public boolean canVisitAllRooms(List<List<Integer>> adj) {
-        int n= adj.size();
+    public void dfs(int node,List<List<Integer>> adj,boolean[] vist,int n){
+        vist[node]=true;
+        for(int keys:adj.get(node)){
+            if(!vist[keys]){
+                dfs(keys,adj,vist,n);
+            }
+        }
+    }
+    public boolean canVisitAllRooms(List<List<Integer>> adj ) {
+        int n=adj.size();
         boolean[] vist= new boolean[n];
-        vist[0]=true; // this are the 1st Room that are be Open o key required. Entry point so the its will be true by default
-        bfs(0,adj,vist);
-        for(boolean ans: vist){
+        dfs(0,adj,vist,n);
+        for(boolean ans:vist){
             if(ans==false){
                 return false;
             }
         }
         return true;
-    }
-    public void bfs(int start, List<List<Integer>> adj, boolean[] vist){
-       for(int ele: adj.get(start)){
-        if(!vist[ele]){
-            vist[ele]=true;
-            bfs(ele,adj,vist);
-        }
-       }
-
     }
 }
