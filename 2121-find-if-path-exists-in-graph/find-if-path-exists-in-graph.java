fa26042fa05fc437b7a05c6893fecca80e8cf3cc
Shubham <shubh386a@gmail.com>
2026-03-10 14:31:00 +0530

    Time: 162 ms (27.99%) | Memory: 262.3 MB (81.68%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,34 +1,36 @@
 class Solution {
-    public void bfs(int n,  List<List<Integer>> adj,int a , int b, boolean[] vist){
+    public void bfs(int start,List<List<Integer>> adj,boolean[] vist){
         Queue<Integer> q= new LinkedList<>();
-        q.add(a);
+        q.add(start);
         while (!q.isEmpty()){
-            int front= q.remove();
-            for(int ele:adj.get(front)){
+            int font=q.remove();
+            for(int ele : adj.get(font)){
                 if(!vist[ele]){
                     q.add(ele);
                     vist[ele]=true;
                 }
+                
             }
         }
     }
     public boolean validPath(int n, int[][] edges, int start, int end) {
+        List<List<Integer>> adj= new ArrayList<>();
         boolean[] vist= new boolean[n];
         if(start==end)return true;
-        List<List<Integer>> adj= new ArrayList<>();
-        for(int i=0;i<n;i++){
+        // iska size n rkhna hai aur n empthy list dalna hai isme
+        for (int i = 0; i < n; i++) {
             List<Integer> list= new ArrayList<>();
-            adj.add(list);// add Empthy list
+            adj.add(list);// insert Empty list inside the adj
+    
         }
-        for(int i=0;i<edges.length;i++){
-            int a=edges[i][0] , b=edges[i][1];
+        for (int i = 0; i < edges.length; i++) {
+            //adj me colum do hi hai bus
+            int a=edges[i][0], b=edges[i][1]; // becouse give is {{1,0},{2,4}} so we travel on this
             adj.get(a).add(b);
             adj.get(b).add(a);
-        }
-        vist[start]=true;
-        bfs(n,adj,start,end,vist);
-        
-        return vist[end];
 
+        }
+        bfs(start,adj,vist);
+        return vist[end];
     }
 }
