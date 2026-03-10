21abb35430e3acfe8563a456b3f3bf8c42f3deaa
Shubham <shubh386a@gmail.com>
2026-03-10 11:13:47 +0530

    Time: 1 ms (88.19%) | Memory: 47.4 MB (56.43%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,15 +1,11 @@
 class Solution {
     public void bfs(int node,int[][] adj,boolean[] vist,int n){
         vist[node]=true;
-        Queue<Integer> q= new LinkedList<>();
-        q.add(node);
-        while(q.size()>0){
-            int font=q.remove();
-            for(int j=0;j<n;j++){
-                if(adj[font][j]==1 && !vist[j]){
-                    q.add(j);
-                    vist[j]=true;
-                }
+        for(int j=0;j<n;j++){
+            if(adj[node][j]==1 &&!vist[j]){
+              
+                bfs(j,adj,vist,n);
+                 
             }
         }
     }
@@ -19,12 +15,9 @@ class Solution {
         boolean[] vist= new boolean[n];
         for(int i=0;i<n;i++){
             if(!vist[i]){
-
-       
                 bfs(i,isConnected,vist,n);
                 count++;
-            }
-            
+            } 
         }
         return count;
     }
