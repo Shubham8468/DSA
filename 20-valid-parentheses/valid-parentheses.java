80c58ee287d01c71f824a8cf7de9a468c827122a
Shubham <shubh386a@gmail.com>
2026-04-02 17:05:56 +0530

    Time: 4 ms (41.15%) | Memory: 43 MB (89.24%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st= new Stack<>();
+        
         for( char ch : s.toCharArray()){
             if(ch == '(' ||ch =='{'|| ch =='['){
                 st.push(ch);
