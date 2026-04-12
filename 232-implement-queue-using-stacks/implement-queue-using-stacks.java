511ae9b5181a39cbfcbfd1423a86499848c44ba9
Shubham <shubh386a@gmail.com>
2026-04-12 18:33:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (27.53%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,48 +1,39 @@
 class MyQueue {
-    Stack<Integer> s1=new Stack<>();
-    Stack<Integer> s2=new Stack<>();
-    public MyQueue() {
-        
-    }
-    
-    public void push(int x) {
-        if(s1.size()==0){
+      Stack<Integer> s1= new Stack<>();
+      Stack<Integer> s2= new Stack<>();
+        public MyQueue() {
+
+        }
+
+        public void push(int x) {
             s1.push(x);
         }
-        else{
-            while(s1.size()>0){
+
+        public int pop() {
+            while(!s1.isEmpty()){
                 s2.push(s1.pop());
             }
-            s1.push(x);
-            while(s2.size()>0){
-                 s1.push(s2.pop());
+            int item=s2.pop();
+            while (!s2.isEmpty()){
+                s1.push(s2.pop());
             }
+            return item;
         }
-    }
-    
-    public int pop() {
-        return s1.pop();
-    }
-    
-    public int peek() {
-        return s1.peek();
-    }
-    
-    public boolean empty() {
-        if(s1.size()==0){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-}
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
+        public int peek() {
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            int item=s2.peek();
+            while (!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+            return item;
+
+        }
+
+        public boolean empty() {
+            return s1.isEmpty();
+
+        }
+    }
