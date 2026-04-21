637ba67f5254cc5d2c0b2908b58e94f749f489a2
Shubham <shubh386a@gmail.com>
2026-04-21 16:26:50 +0530

    Time: 1 ms (7.64%) | Memory: 43.6 MB (14.95%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -14,21 +14,45 @@
  * }
  */
 class Solution {
-     public int sumOfLeftLeaves(TreeNode root) {
-       if(root==null) return 0;
-       Queue<TreeNode>que=new LinkedList<>();
-       que.add(root);
-       int sum=0;
-
-       while(!que.isEmpty()){
-          TreeNode curr=que.poll();
-          if(curr.left!=null){
-             if(curr.left.left==null && curr.left.right==null)sum+=curr.left.val;
-             que.add(curr.left);
-          }
-          if(curr.right!=null)que.add(curr.right);
-       }
-
-       return sum;
+     public class  Pair{
+        TreeNode root;
+        boolean isLeft;
+        Pair(TreeNode root, boolean isLeft){
+            this.root=root;
+            this.isLeft=isLeft;
+        }
     }
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        
+        return bfs(root);
+    }
+    public int bfs(TreeNode root){
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(root,false));
+        int sum=0;
+        while (!q.isEmpty()){
+            int size=q.size();
+            for (int i = 1; i <=size ; i++) {
+                Pair temp=q.poll();
+                TreeNode node=temp.root;
+                int item= temp.root.val;
+                if(temp.isLeft && node.left==null && node.right==null){
+                    sum+=item;
+                }
+                if(node.right!=null){
+                    q.add(new Pair(node.right,false));
+                }
+                if(node.left!=null){
+                    q.add(new Pair(node.left,true));
+                }
+
+
+            }
+        }
+        return sum;
+    }
+    
 }
