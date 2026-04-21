08dc48467d5b82f0424655cb39115f635a25332a
Shubham <shubh386a@gmail.com>
2026-04-21 16:07:38 +0530

    Time: 1 ms (7.64%) | Memory: 43.5 MB (14.95%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -14,46 +14,49 @@
  * }
  */
 class Solution {
-    public class  Pair{
-        TreeNode root;
-        boolean isLeft;
-        Pair(TreeNode root, boolean isLeft){
-            this.root=root;
-            this.isLeft=isLeft;
-        }
-    }
-    public int sumOfLeftLeaves(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-       
-       return  bfs(root);
-       
-    }
-    public int  bfs(TreeNode root){
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(root,false));
-        int sum=0;
-        while (!q.isEmpty()){
-            int size=q.size();
-            for (int i = 1; i <=size ; i++) {
-                Pair temp=q.poll();
-                TreeNode node=temp.root;
-                int item= node.val;
-                if(temp.isLeft && node.left==null && node.right==null){
-                    sum=sum+item;
-                }
-                
-                if(node.left!=null){
-                    q.add(new Pair(node.left,true));
-                }
-                if(node.right!=null){
-                    q.add(new Pair(node.right,false));
-                }
+   public class Pair{
+TreeNode root;
+boolean isLeft;
+Pair(TreeNode root, boolean isLeft){
+this.root=root;
+this.isLeft=isLeft;
+}
+}
+public int sumOfLeftLeaves(TreeNode root) {
+if(root==null){
+return 0;
+}
 
 
-            }
-        }
-        return sum;
-    }
+return bfs(root);
+
+
+}
+public int bfs(TreeNode root){
+Queue<Pair> q=new LinkedList<>();
+int sum=0;
+q.add(new Pair(root,false));
+while (!q.isEmpty()){
+int size=q.size();
+for (int i = 1; i <=size ; i++) {
+Pair temp=q.poll();
+TreeNode node=temp.root;
+int item= temp.root.val;
+if(temp.isLeft && node.left==null && node.right==null){
+sum+=item;
+}
+if(node.right!=null){
+q.add(new Pair(node.right,false));
+}
+if(node.left!=null){
+q.add(new Pair(node.left,true));
+}
+
+
+}
+}
+return sum;
+}
+
+
 }
