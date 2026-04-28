cff20bee70c188e8e0e632d8d2ed916b5d733f2d
Shubham <shubh386a@gmail.com>
2026-04-28 17:45:24 +0530

    Time: 100 ms (12.97%) | Memory: 81.1 MB (84.19%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,60 +1,15 @@
 class Solution {
-    public void meresort(int[] arr){
-        int n=arr.length;
-        if(n==1){
-            return ;
+    public int[] sortArray(int[] nums) {
+        
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
+        for(int ele : nums){
+            pq.add(ele);
         }
-        int idx=0;
-        int[] a=new int[n/2];
-        int[] b=new int[n-n/2];
-        //add item in this arrays
-        for(int i=0;i<a.length;i++){
-            a[i]=arr[idx];
-            idx++;
-        }
-        for(int i=0;i<b.length;i++){
-            b[i]=arr[idx];
-            idx++;
-        }
-        // doing magig
-        meresort(a);
-        meresort(b);
-        //magre a and b
-        mergearr(a,b,arr);
+        int n=pq.size();
+        for(int i=0;i<n;i++){
 
-    }
-    public void mergearr(int[] a,int[] b,int[] arr){
-        int i=0,j=0,k=0;
-        while(i<a.length&& j<b.length){
-            if(a[i]<b[j]){
-                arr[k]=a[i];
-                i++;
-            }
-            else{
-                arr[k]=b[j];
-                j++;
-            }
-            k++;
+          nums[i]=pq.remove();
         }
-        while(i<a.length){
-            arr[k]=a[i];
-            i++;
-            k++;
-        }
-        while(j<b.length){
-            arr[k]=b[j];
-            j++;
-            k++;
-        }
-
-    }
-    public int[] sortArray(int[] arr) {
-        //apply merege sort 
-        if(arr.length==1){
-            return arr;
-        }
-        meresort(arr);
-        return arr;
-       
+        return nums;
     }
 }
