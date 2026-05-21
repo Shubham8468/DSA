6207461c05736d294ede0f2cd6ba9ebad66585b8
Shubham <shubh386a@gmail.com>
2026-05-21 18:17:14 +0530

    Time: 4 ms (62.29%) | Memory: 50.2 MB (70.85%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -2,22 +2,17 @@ class Solution {
     public int minPathSum(int[][] arr) {
         int m=arr.length;
         int n=arr[0].length;
-        int[][] dp=new int[m][n];
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[0][0]=arr[0][0];
-                }
-                else if(i==0){
-                    dp[i][j]=arr[i][j]+dp[i][j-1];
-                }
-                else if(j==0){
-                    dp[i][j]=arr[i][j]+dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=arr[i][j]+ Math.min(dp[i-1][j],dp[i][j-1]);
-                }
+        int[][] dp= new int[m][n];
+        dp[0][0]=arr[0][0];
+        for(int j=1;j<n;j++){
+            dp[0][j]=arr[0][j]+dp[0][j-1];
+        }
+        for(int i=1;i<m;i++){
+            dp[i][0]=arr[i][0]+dp[i-1][0];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=arr[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
             }
         }
         return dp[m-1][n-1];
